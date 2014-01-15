007445ab12cd02b4b9945b73952d1c28680de39e
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-01-15 12:19:39 +0100

    Move

--- js/widget_table.js
+++ js/widget_table.js
@@ -62,5 +62,61 @@
 					.duration(100)
 					.style('height', '20px');
 		};
+
+		this.move = function(p) {
+			var tr = this.body.selectAll('tr.selected');
+			if (tr.empty()) {
+				return;
+			}
+
+			var data = [];
+			var old_data = tr.datum();
+			var old_div = tr.selectAll('div');
+
+			old_div[0].forEach(function(d, i) {
+				var width = d.getBoundingClientRect().width;
+				data[i] = { width: width, data: old_data[i] };
+			});
+
+			var bbox = tr.node().getBoundingClientRect();
+
+			var div = d3.select('body').append('div').classed('moving_box', true);
+			div
+				.style('top', bbox.top + 'px')
+				.style('left', bbox.left + 'px')
+				.style('width', bbox.width + 'px')
+				.style('height', bbox.height + 'px');
+
+			var moving_tr = div.append('table')
+				.classed('ocp_table', true)
+				.append('tr')
+					.classed('ocp_row', true);
+
+			var td = moving_tr.selectAll('td').data(data);
+			var new_td = td.enter().append('td');
+			new_td.append('div')
+				.classed('cell', true)
+				.style('height', '20px')
+				.style('width', function(d) {
+					return d.width + 'px';
+				})
+				.text(function(d) { return d.data; });
+
+			tr.style('opacity', 0);
+
+			div
+				.transition()
+					.duration(2000)
+					.style('top', p.y + 'px')
+					.style('left', p.x + 'px')
+					.each('end', function() {
+						self.removeRecord();
+						self.repaint();
+					})
+				.transition()
+					.duration(500)
+					.style('opacity', 0)
+				.remove();
+		};
 	};
 })( jlg );
